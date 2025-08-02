89a57184e659b080704e54a5a2357ec4adf99ad2
Ashish Sharma <154233161+ashish-ecloudbridge@users.noreply.github.com>
2025-08-02 22:06:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/ecloudbridge/", // important for GitHub Pages subpath
+  base: "/ecloudbridge/", // for GitHub Pages
   server: {
     host: "::",
     port: 8080,
