64eafc12a7955288e48771832839e5d3f4a867b1
Ashish Sharma <154233161+ashish-ecloudbridge@users.noreply.github.com>
2025-08-02 22:13:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // for GitHub Pages
+  base: "/ecloudbridge/", // required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
